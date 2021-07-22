f0e0ef23698ecc227e942009a7837f7db07a9ccf
Linh <linh@Linhs-MacBook-Pro.local>
2021-07-22 08:49:30 -0700

    return university names when route to university/:country

--- src/routes/university.js
+++ src/routes/university.js
@@ -4,9 +4,16 @@ const router = express.Router();
 
 router.get('/:country', async function(req, res, next) {
     const country = req.params.country ?? '';
-    const allUnis = await basicModule.getAllUniversities(country);
+    try {
+        const allUnis = await basicModule.getAllUniversities(country);
+        const names = allUnis.map((university) => {
+            return university.name
+        })
+        res.json({ data: names });
+    } catch (e) {
+        throw new Error("The country provided doesn't exist or has no Universities. Please try again")
+    }
 
-    res.json({ data: allUnis });
 });
 
 module.exports = router;
